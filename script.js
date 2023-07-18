03d7ae2151bb768eee080adab167daada9a193ea
AndreaLinza <andrealinza@gmail.com>
2023-07-19 01:22:36 +0200

    neanche iniziato

---
+++ script.js
@@ -0,0 +1,92 @@
+const post = [
+
+    {
+        id: 1,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/300?image=171",
+        author: {
+            name: "Phil Mangione",
+            image: "https://unsplash.it/300/300?image=15"
+        },
+        likes: 80,
+        created: "2021-06-25"
+    },
+    {
+        id: 2,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=112",
+        author: {
+            name: "Sofia Perlari",
+            image: "https://unsplash.it/300/300?image=10"
+        },
+        likes: 120,
+        created: "2021-09-03"
+    },
+    {
+        id: 3,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=234",
+        author: {
+            name: "Chiara Passaro",
+            image: "https://unsplash.it/300/300?image=20"
+        },
+        likes: 78,
+        created: "2021-05-15"
+    },
+    {
+        id: 4,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=24",
+        author: {
+            name: "Luca Formicola",
+            image: null
+        },
+        likes: 56,
+        created: "2021-04-03"
+    },
+    {
+        id: 5,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=534",
+        author: {
+            name: "Alessandro Sainato",
+            image: "https://unsplash.it/300/300?image=29"
+        },
+        likes: 95,
+        created: "2021-03-05"
+    }
+]
+
+
+
+
+post.forEach(element => {
+    const postContainer = document.createElement("div")
+    postContainer.classList.add("post")
+
+    const postHeader = document.createElement("div")
+    postHeader.classList.add("post__header")
+
+
+
+});
+
+
+post.forEach(element => {
+    
+    const postMeta = document.createElement("div")
+    postMeta.classList.add("post-meta")
+    postMeta.innerHTML = `<div class="post-meta__icon">
+    <img class="profile-pic" src="${element.author.image}" alt="Phil Mangione">                    
+    </div>`
+    
+    postHeader.append(postMeta)
+});
+
+post.forEach(element => {
+    
+    const {}
+});
+
+
+postContainer.append(postHeader)
